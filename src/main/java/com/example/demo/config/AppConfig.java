cc27131eb417984dfc0d2c3b54ba56216f21e933
Vindhyachal Tiwari <vindhyachaltiwari01@gmail.com>
2023-09-26 20:43:24 +0530

    first commit

---
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.config;
+
+import com.example.demo.DeprecatedProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class AppConfig {
+
+    private String name;
+    private String version;
+    private String description;
+
+    // Constructor, getters, and setters
+
+    // Initialize properties from JSON file
+    public AppConfig() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Resource resource = new ClassPathResource("application.json");
+        try (InputStream inputStream = resource.getInputStream()) {
+            JsonNode jsonNode = objectMapper.readTree(inputStream);
+            Map<String, String> properties = objectMapper.convertValue(jsonNode, Map.class);
+            Field[] fields = this.getClass().getDeclaredFields();
+
+            for (Field field : fields) {
+                String fieldName = field.getName();
+                if (properties.containsKey(fieldName)) {
+                    field.setAccessible(true);
+                    field.set(this, properties.get(fieldName));
+                    if (field.isAnnotationPresent(DeprecatedProperty.class)) {
+                        System.out.println("Warning: Property '" + fieldName + "' is deprecated.");
+                    }
+                }
+            }
+        } catch (IOException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
